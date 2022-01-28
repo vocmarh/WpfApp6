1be81001bf86842a53475ff14912e3847f37abce
Сергей Храмцов <vocmarh@mail.ru>
2022-01-28 19:16:05 +0500

    Добавьте файлы проекта.

---
+++ WpfApp6/WeatherControl.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp6
+{
+    class WeatherControl : DependencyObject
+    {
+        public string WindDirection { get; set; }
+
+        public int WindSpeed { get; set; }
+
+        public enum Railfall
+        {
+            Солнечно = 0,
+            Облачно = 1,
+            Дождь = 2,
+            Снег = 3
+        }
+
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+        public static readonly DependencyProperty TemperatureProperty;
+
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)),
+                new ValidateValueCallback(ValidateTemperature));
+        }
+
+        public static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
